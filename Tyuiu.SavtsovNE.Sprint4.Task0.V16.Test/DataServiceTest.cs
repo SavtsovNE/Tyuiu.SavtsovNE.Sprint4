2b19c05ba962e0957c05e1a1c654925386d05c5c
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-18 23:16:09 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.SavtsovNE.Sprint4.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.SavtsovNE.Sprint4.Task0.V16.Lib;
+namespace Tyuiu.SavtsovNE.Sprint4.Task0.V16.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+            [TestMethod]
+            public void ValidGetMultEvenArrEl()
+            {
+                DataService ds = new DataService();
+                int[] array = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+                int res = ds.GetMultEvenArrEl(array);
+                int wait = 3072;
+                Assert.AreEqual(wait, res);
+            }
+        
+    }
+}
---
+++ Tyuiu.SavtsovNE.Sprint4.Task0.V16/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.SavtsovNE.Sprint4.Task0.V16.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint4.Task0.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4   |  Савцов Никита Евгеньевич | ИБКСБ - 24 - 1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #4                                                     *");
+            Console.WriteLine("* Тема: Одноименные массивы (статический ввод).                 *");
+            Console.WriteLine("* Задание #0                                                    *");
+            Console.WriteLine("* Вариант #16                                                   *");
+            Console.WriteLine("* Савцов Никита Евгеньевич | ИБКСБ - 24 - 1                     *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      *");
+            Console.WriteLine("* Написать программу, которая считает произведение четных       *");
+            Console.WriteLine("* элементов массива.                                            *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine("*****************************************************************");
+
+            DataService ds = new DataService();
+
+            int[] array = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("Результат:" + ds.GetMultEvenArrEl(array));
+
+            Console.ReadKey();
+        }
+    }
+}
