4c79de3d74350dc07e7184f18dc3397e008336fd
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-18 23:51:23 +0500

    Task1

---
+++ Tyuiu.SavtsovNE.Sprint4.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SavtsovNE.Sprint4.Task1.V11.Lib
+{
+    public class DataService : ISprint4Task1V11
+    {
+        public int Calculate(int[] array)
+        {
+            int res = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    res += array[i];
+                }
+            }
+            return res;
+        }
+    }
+}
