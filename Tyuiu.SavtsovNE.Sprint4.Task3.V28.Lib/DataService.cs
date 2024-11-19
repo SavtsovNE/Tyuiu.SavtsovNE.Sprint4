2b78e7f7b304e494fa9079c2854bddccfd4d271c
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-19 21:46:36 +0500

    Task3

---
+++ Tyuiu.SavtsovNE.Sprint4.Task3.V28.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.SavtsovNE.Sprint4.Task3.V28.Lib
+{
+    public class DataService : ISprint4Task3V28
+    {
+        public int Calculate(int[,] array)
+        {
+            int columns = 3;
+            int min = array[0, columns];
+
+            for (int rows = 0; rows < array.GetLength(0); rows++)
+            {
+                int tpm = array[rows, columns];
+
+                if (tpm < min)
+                {
+                    min = tpm;
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Tyuiu.SavtsovNE.Sprint4.Task3.V28/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.SavtsovNE.Sprint4.Task3.V28.Lib;
+namespace Tyuiu.SavtsovNE.Sprint4.Task3.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3|Выполнил: Савцов Никита Евгеньевич | ИБКСБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил Выполнил: Савцов Никита Евгеньевич | ИБКСБ-24-1                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 4 до 9.                          *");
+            Console.WriteLine("* Найдите минимальный элемент в четвертом столбце массива.                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:                                                                    ");
+            Console.WriteLine("{ 5, 4, 9, 8, 5 },\r\n{ 4, 6, 6, 9, 9 },\r\n{ 9, 8, 4, 8, 8 },\r\n{ 6, 8, 6, 8, 9 },\r\n{ 9, 4, 6, 6, 7 }");
+
+            int[,] array = {
+            {5,4,9,8,5 },
+            {4,6,6,9,9 },
+            {9,8,4,8,8 },
+            {6,8,6,8,9 },
+            {9,4,6,6,7 } };
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(array);
+
+            Console.WriteLine("Минимальный элемент в четвертом столбце массива: " + res);
+            Console.ReadKey();
+        }
+    }
+}
