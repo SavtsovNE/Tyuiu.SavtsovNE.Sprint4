a4443b838aec4155302740c6d24b30e4760a426c
Никита Савцов <nsavtsov89@inbox.ru>
2024-11-20 02:13:12 +0500

    Task5

---
+++ Tyuiu.SavtsovNE.Sprint4.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SavtsovNE.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.SavtsovNE.Sprint4.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+
+            int[,] mtrx = new int[2, 2] { {-5, 2 },
+                                          { 2, 1} };
+            int[,] res = new int[2, 2] { {-5, 1 },
+                                         { 1, 1} };
+
+            CollectionAssert.AreEqual(ds.Calculate(mtrx), res);
+        }
+    }
+}
